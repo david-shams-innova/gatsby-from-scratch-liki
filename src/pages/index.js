eb1f815e5ba08c109326f294d9f0b0e207e455de
David Shams <david.shams.innova@gmail.com>
2022-07-07 15:54:28 -0500

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
-import * as React from "react"
+// import * as React from "react"
 
-export default IndexPage = () => {
+// export default IndexPage = () => {
+//   return (
+//     <main style={pageStyles}>
+//      <h3>Coming Soon!</h3>
+//     </main>
+//   )
+// }
+
+
+import React from 'react'
+
+export default function index() {
   return (
-    <main style={pageStyles}>
-     <h3>Coming Soon!</h3>
-    </main>
+    <div>index</div>
   )
 }
-
